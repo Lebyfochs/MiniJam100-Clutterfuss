06a76f1c827208def7dd3858042a8bffaa8729d1
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2022-02-18 19:28:35 +0000

    AI movement added.
    Added AI movement code.

--- MJ100_Clutterfuss/Assets/Scripts/AI/MoveState.cs
+++ MJ100_Clutterfuss/Assets/Scripts/AI/MoveState.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//AI movement code similar to Idle state from previous projects.
+//Used for when worker is not at desk working or doing anything important.
 public class MoveState : MonoBehaviour, IState
 {
 
     public WorkerAgent worker;
+    public float NavRange = 10.0f;
+    [SerializeField] private List<Transform> MovPosTrans = new List<Transform>();
+
+    private void Awake()
+    {
+        worker = GetComponent<WorkerAgent>();
+    }
 
     void Start()
     {
@@ -21,6 +31,17 @@ public class MoveState : MonoBehaviour, IState
 
     public IEnumerator Execute()
     {
-        yield return null;
+        while(true)
+        {
+            var newPos = MovPosTrans[Random.Range(0, MovPosTrans.Count)];
+            worker.navAI.destination = newPos.position;
+
+            while (Vector3.Distance(transform.position, worker.navAI.destination) < 0.5f)
+            {
+                yield return null;
+            }
+            yield return new WaitForSeconds(3.0f);
+        }
+        
     }
 }
