00c44f478348c9fb79877cc15b9e2370843cbbc0
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2022-02-20 00:20:06 +0000

    Working Text
    Working text mechanic for displaying Chaos level.

---
+++ MJ100_Clutterfuss/Assets/Scripts/TextHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour
+{
+    public Text ChaosText;
+    ChaosHandler chaos;
+
+
+    private void Awake()
+    {
+        chaos = GameObject.Find("ChaosHandlingOBJ").GetComponent<ChaosHandler>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChaosText.text = chaos.Chaos.ToString();
+    }
+}
